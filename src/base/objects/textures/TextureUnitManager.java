916b0c71dd2c0860b4fc3b395a177dcac499d95c
Jovan Djukic <djukicjovan56@gmail.com>
2018-02-25 15:35:33 +0100

    Added a texture uit manager for dynamic texture unit allocation.

---
+++ src/base/objects/textures/TextureUnitManager.java
@@ -0,0 +1,40 @@
+package base.objects.textures;
+
+import com.jogamp.opengl.GL4;
+
+public class TextureUnitManager {
+	private static class Constants {
+		public static int maximumNubmeerOfTextures = 8;
+	}
+	private static TextureUnitManager instance;
+	
+	public static TextureUnitManager getInstance() {
+		if (TextureUnitManager.instance == null) {
+			TextureUnitManager.instance = new TextureUnitManager();
+		}
+		return instance;
+	}
+	
+	private boolean isTextureUnitTaken[];
+	
+	public TextureUnitManager() {
+		this.isTextureUnitTaken = new boolean[Constants.maximumNubmeerOfTextures];
+	}
+	
+	public int getTextureUnit() {
+		int textureUnit = -1;
+		
+		for (int i = 0; i < this.isTextureUnitTaken.length; i++) {
+			if (!this.isTextureUnitTaken[i]) {
+				this.isTextureUnitTaken[i] = true;
+				textureUnit = i;
+				break;
+			}
+		}
+		return GL4.GL_TEXTURE0 + textureUnit;
+	}
+	
+	public void freeTextureUnit(int textureUnit) {
+		this.isTextureUnitTaken[textureUnit - GL4.GL_TEXTURE0] = false;
+	}
+}
